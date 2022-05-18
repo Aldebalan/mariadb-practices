06fb11d44a1ee39466c7718e3fe70f249ad03445
Aldebalan <tjdgus4093@naver.com>
2022-05-18 11:30:49 +0900

    jdbc #4

---
+++ jdbc-practice/sql/employee-department.sql
@@ -0,0 +1,18 @@
+-- Department:findAll
+select no, name
+  from department
+  order by no desc;
+
+-- EmployeeDao:findAll
+select no, name, department_no
+  from employee
+  order by no desc;
+
+-- EmployeeDao:delete()
+delete from employee;
+
+-- DepartmentDao:update
+update department
+  set name = '솔루션개발'
+  where no=3;
+  
--- jdbc-practice/src/main/java/test/DeleteTest01.java
+++ jdbc-practice/src/main/java/test/DeleteTest01.java
@@ -9,12 +9,52 @@ public class DeleteTest01 {
 
 	public static void main(String[] args) {
 
-		delete(5L);
-		delete(6L);
-		delete(7L);
+//		delete(5L);
+//		delete(6L);
+//		delete(7L);
+		
+		delete();
 	}
 
-	private static boolean delete(Long no) {
+	private static void delete() {
+		boolean result = false;
+
+		Connection connection = null;
+		Statement stmt = null;
+		try {
+			// 1. JDBC Driver 로딩 (JDBC Class 로딩: class loader)
+			Class.forName("org.mariadb.jdbc.Driver");
+
+			// 2. 연결하기
+			String url = "jdbc:mysql://192.168.10.31:3306/webdb?charset=utf8";
+			connection = DriverManager.getConnection(url, "webdb", "webdb");
+
+			// 3. Statment 생성
+			stmt = connection.createStatement();
+
+			// 4. SQL 실행
+			String sql = "delete from department";
+			stmt.executeUpdate(sql);
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} catch (SQLException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		} finally {
+			try {
+				if (stmt != null) {
+					stmt.close();
+				}
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private static boolean deleteAll(Long no) {
 		boolean result = false;
 
 		Connection connection = null;
@@ -53,7 +93,6 @@ public class DeleteTest01 {
 		return result;
 	}
 
-	
 	
 	
 }
